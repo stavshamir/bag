d9a6d7293f044f10358a47e73da3a505e99f9eca
Stav Shamir <shamir.stav@gmail.com>
2018-05-31 10:14:54 +0300

    added equals and hashCode

--- src/main/java/alias/Alias.java
+++ src/main/java/alias/Alias.java
@@ -1,9 +1,25 @@
 package alias;
 
+import java.util.Objects;
+
 public class Alias {
     private String name;
     private String value;
     private String description;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Alias alias = (Alias) o;
+        return Objects.equals(name, alias.name) &&
+                Objects.equals(value, alias.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, value);
+    }
 
     public Alias(String name, String value, String description) {
         this.name = name;
--- src/test/java/alias/AliasTest.java
+++ src/test/java/alias/AliasTest.java
@@ -7,18 +7,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AliasTest {
 
     @Test
-    public void testToString() {
-        Alias testAlias = new Alias("grep", "grep --color=auto", "grep with colors");
+    public void testEquals() {
+        Alias aliasWithDescription  = new Alias("grep", "grep --color=auto", "grep with colors");
+        Alias aliasEmptyDescription = new Alias("grep", "grep --color=auto", "");
+        Alias aliasNullDescription  = new Alias("grep", "grep --color=auto");
 
-        assertThat(testAlias.toString())
+        assertThat(aliasWithDescription)
+                .isEqualTo(aliasWithDescription);
+
+        assertThat(aliasWithDescription)
+                .isEqualTo(aliasEmptyDescription);
+
+        assertThat(aliasWithDescription)
+                .isEqualTo(aliasNullDescription);
+    }
+
+    @Test
+    public void testToString() {
+        Alias alias = new Alias("grep", "grep --color=auto", "grep with colors");
+
+        assertThat(alias.toString())
                 .isEqualTo("grep='grep --color=auto' # grep with colors");
     }
 
     @Test
     public void testToString_no_description() {
-        Alias testAlias = new Alias("grep", "grep --color=auto", "");
+        Alias aliasEmptyDescription = new Alias("grep", "grep --color=auto", "");
+        assertThat(aliasEmptyDescription.toString())
+                .isEqualTo("grep='grep --color=auto'");
 
-        assertThat(testAlias.toString())
+        Alias aliasNullDescription = new Alias("grep", "grep --color=auto");
+        assertThat(aliasNullDescription.toString())
                 .isEqualTo("grep='grep --color=auto'");
     }
 }
