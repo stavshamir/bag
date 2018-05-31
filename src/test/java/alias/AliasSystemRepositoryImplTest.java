d92c676c2e01de828b09ae9ebb0614a3e5d48d20
Stav Shamir <shamir.stav@gmail.com>
2018-05-31 15:13:06 +0300

    implemented getAllAliases

--- src/test/java/alias/AliasSystemRepositoryImplTest.java
+++ src/test/java/alias/AliasSystemRepositoryImplTest.java
@@ -5,24 +5,18 @@ import org.junit.Test;
 import java.io.IOException;
 import java.util.Set;
 
-import static junit.framework.TestCase.fail;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
 
 public class AliasSystemRepositoryImplTest {
 
     private AliasSystemRepository aliasSystemRepository = new AliasSystemRepositoryImpl();
 
     @Test
-    public void getAliases() {
-        try {
-            Set<Alias> aliases = aliasSystemRepository.getAliases();
+    public void getAliases() throws IOException {
+        Set<Alias> aliases = aliasSystemRepository.getAliases();
 
-            assertThat(aliases)
-                    .contains(new Alias("ls", "ls --color=auto"));
-        } catch (IOException e) {
-            e.printStackTrace();
-            fail();
-        }
+        assertThat(aliases)
+                .contains(new Alias("ls", "ls --color=auto"));
     }
+
 }
