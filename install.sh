1f76226ee3e385a7eca63bc4fe1471027cb2d428
Stav Shamir <shamir.stav@gmail.com>
2018-06-07 10:09:38 +0300

    updated install.sh

--- install.sh
+++ install.sh
@@ -17,12 +17,12 @@ alias-script=${BAG_HOME}/alias.sh
 EOF
 
 echo 'creating alias for bag'
-alias bag='java -jar $BAG_HOME/bin/bag-1.0-SNAPSHOT.jar'
+alias bag='java -jar $BAG_HOME/bag-1.0-SNAPSHOT.jar'
 
 echo 'adding lines to .bashrc'
 grep -qF '# bag - bash alias' ~/.bashrc || echo '# bag - bash alias generator' >> ~/.bashrc
 grep -qF 'export BAG_HOME'    ~/.bashrc || echo export BAG_HOME=${BAG_HOME}    >> ~/.bashrc
 grep -qF 'source '${BAG_HOME} ~/.bashrc || echo source ${BAG_HOME}/aliases     >> ~/.bashrc
-grep -qF 'alias bag='         ~/.bashrc || echo alias bag='java -jar $BAG_HOME/bin/bag-1.0-SNAPSHOT.jar' >> ~/.bashrc
+grep -qF 'alias bag='         ~/.bashrc || echo alias bag='java -jar $BAG_HOME/bag-1.0-SNAPSHOT.jar' >> ~/.bashrc
 
 echo 'done'
